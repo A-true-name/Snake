30efbb5758b9514a3aa2958acdbff8ca477bb53c
kaufmha <kaufmha@whs.school.nz>
2024-05-27 10:09:35 +1200

    Signed-off-by: kaufmha <kaufmha@whs.school.nz>

--- Snake.java
+++ Snake.java
@@ -3,7 +3,7 @@
  * @author (Hamish Kaufman)
  * @version (a version number or a date)
  */
-public class Snake
+public class Snake//ba435bsedrtgybaert4b
 {
     // instance variables - replace the example below with your own
     private int x;
@@ -15,6 +15,10 @@ public class Snake
     {
         // initialise instance variables
         x = 0; // skibidi sigma Zack was here.
-        
+        //q2e4ertaegeaRRAARG
     }
-}
+}//AERTARRGEGARAERY
+//56UYSA567YHNS
+//AQW435RGBERTFH
+//XZ5YN6S5REYNw567ns5rny7
+//dr4567ynserh5ynsrthd6um
